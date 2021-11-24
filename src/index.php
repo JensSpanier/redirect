4fe15a927d77fd0cbfa3284ac3d36aeae7ffbfac
Jens Spanier <j.spanier@digital-h.de>
2021-11-24 09:27:05 +0100

    Remove hash from css ref

--- src/index.php
+++ src/index.php
@@ -17,7 +17,7 @@ if ($original_host !== HOME) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 
     <title>Weiterleitung</title>
 </head>
