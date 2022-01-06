bd27a78d93c0e8dd742f209cfbb0190f49e51cbe
Jens Spanier <j.spanier@digital-h.de>
2022-01-06 09:48:52 +0100

    Update index

--- src/index.php
+++ src/index.php
@@ -1,13 +1,32 @@
 <?php
 $env_redirect_home = getenv('REDIRECT_HOME');
-define('HOME', $env_redirect_home === false ? '127.0.0.1' : $env_redirect_home);
-require_once __DIR__ . '/redirect.php';
+define('HOME', $env_redirect_home === false ? 'localhost' : $env_redirect_home);
 
 $original_host = explode(':', $_SERVER['HTTP_HOST'])[0];
 if ($original_host !== HOME) {
     $txt_records = dns_get_record("_redirect.$original_host", DNS_TXT);
+    require_once __DIR__ . '/redirect.php';
     new Redirect($txt_records);
 }
+
+function _(string $text): void
+{
+    echo htmlspecialchars($text);
+}
+
+$ipv4 = [];
+$ipv6 = [];
+foreach (dns_get_record(HOME, DNS_A | DNS_AAAA) as $record) {
+    switch ($record['type']) {
+        case 'A':
+            $ipv4[] = $record['ip'];
+            break;
+        case 'AAAA':
+            $ipv6[] = $record['ipv6'];
+            break;
+    }
+}
+
 ?>
 <!doctype html>
 <html lang="de">
@@ -27,21 +46,23 @@ if ($original_host !== HOME) {
     <div class="container">
         <h1>Weiterleitung</h1>
         <p>
-            Lege einen CNAME-Eintrag für die Domain an, die du weiterleiten möchtest. Dieser muss auf <code>redirect.spnr.de</code> zeigen.
+            Lege einen CNAME-Eintrag für die Domain an, die du weiterleiten möchtest. Dieser muss auf <code><?php _(HOME); ?></code> zeigen.
             <br>
             Beispiel:
             <br>
-            <code>www.example.com IN CNAME redirect.spnr.de</code>
-            <br>
-            Oder verwende die IP-Adresse <code>82.165.96.230</code>, falls du keinen CNAME anlegen kannst.
-            <br>
-            Beispiel:
-            <br>
-            <code>example.com IN A 82.165.96.230</code>
-            <br>
-            IPv6 wird auch unterstützt:
-            <br>
-            <code>example.com IN AAAA 2001:8d8:1801:8546::1</code>
+            <code>www.example.com IN CNAME <?php _(HOME); ?>.</code>
+            <?php if ($ipv4 || $ipv6) : ?>
+                <br>
+                Oder verwende die IP-Adressen, falls du keinen CNAME anlegen kannst:
+                <?php foreach ($ipv4 as $ip) : ?>
+                    <br>
+                    <code>example.com IN A <?php _($ip); ?></code>
+                <?php endforeach; ?>
+                <?php foreach ($ipv6 as $ip) : ?>
+                    <br>
+                    <code>example.com IN AAAA <?php _($ip); ?></code>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </p>
         <hr>
         <p>
